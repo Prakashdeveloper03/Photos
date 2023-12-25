c9585d2f945e49dd2ce1f97d19c8747cb4846b37
Siva Prakash <prakashdeveloper@outlook.in>
2023-12-25 13:14:06 +0530

    Added schema.sql & static folder

--- src/main/java/com/prakashdev/photos/web/PhotosController.java
+++ src/main/java/com/prakashdev/photos/web/PhotosController.java
@@ -1,12 +1,54 @@
 package com.prakashdev.photos.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.prakashdev.photos.model.Photo;
+import com.prakashdev.photos.service.PhotosService;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Collection;
 
 @RestController
 public class PhotosController {
+
+    private final PhotosService photosService;
+
+    public PhotosController(PhotosService photosService) {
+        this.photosService = photosService;
+    }
+
     @GetMapping("/")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/photos")
+    public Iterable<Photo> get() {
+        return photosService.get();
+    }
+
+    @GetMapping("/photos/{id}")
+    public Photo get(@PathVariable Integer id) {
+        Photo photo = photosService.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photos/{id}")
+    public void delete(@PathVariable Integer id) {
+        photosService.remove(id);
+    }
+
+    @PostMapping("/photos")
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        return photosService.save(file.getOriginalFilename(), file.getContentType(), file.getBytes());
     }
 }
